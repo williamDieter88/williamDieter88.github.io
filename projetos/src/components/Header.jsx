9893456a60fc1b62c6bfc4303d7d502b48d6b9c4
William Dieter <67603604+williamDieter88@users.noreply.github.com>
2020-09-17 20:39:27 -0300

    Delete Header.jsx

--- projetos/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Header extends Component {
-  render() {
-    return <header><h1 className="header">Movie Cards Library</h1></header>;
-  }
-}
-
-export default Header;
